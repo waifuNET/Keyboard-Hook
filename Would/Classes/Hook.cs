74ab201956e01b76c6d0cbaca0e82b7a9c07a00e
BuildTools <artem228.tyg@gmail.com>
2019-12-05 20:09:11 +0600

    Добавьте файлы проекта.

---
+++ Would/Classes/Hook.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Would.Classes
+{
+    class Hook
+    {
+        #region Libraries
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc callback, IntPtr hInstance, uint threadId);
+
+        [DllImport("user32.dll")]
+        static extern bool UnhookWindowsHookEx(IntPtr hInstance);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr LoadLibrary(string lpFileName);
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        const int WH_KEYBOARD_LL = 13;
+
+        private static LowLevelKeyboardProc _proc = hookProc;
+
+        private static IntPtr hhook = IntPtr.Zero;
+        #endregion
+
+        public delegate void Function(int key);
+        static Function function = default;
+
+        /// <summary>
+        /// Pass a function with an int parameter that will accept the key pressed.
+        /// </summary>
+        /// <param name="func"></param>
+        public static void Start(Function func)
+        {
+            function = func;
+            IntPtr hInstance = LoadLibrary("User32");
+            hhook = SetWindowsHookEx(WH_KEYBOARD_LL, _proc, hInstance, 0);
+        }
+
+        public static void Stop()
+        {
+            UnhookWindowsHookEx(hhook);
+        }
+
+        public static IntPtr hookProc(int code, IntPtr wParam, IntPtr lParam)
+        {
+            if (code >= 0 && wParam == (IntPtr)0x100 || code >= 0 && wParam == (IntPtr)260)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                function(vkCode);
+            }
+            return IntPtr.Zero;
+        }
+    }
+}
---
+++ Would/View/Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Would.Classes;
+
+namespace Would
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            Hook.Start(test);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Hook.Stop();
+        }
+
+        public static void test(int key)
+        {
+            MessageBox.Show("Нажата клавиша: " + key);
+        }
+    }
+}
